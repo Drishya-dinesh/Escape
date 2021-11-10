b123ef787b85ac0184bc20e87c06ffa47fe8e0c3
Drishya Dinesh <drishyadinesh83@gmail.com>
2021-11-10 07:15:21 -0500

    Test

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
 import { LoginComponent } from './login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'create',
+    component: CreateAccountComponent,
   },
 ];
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { FormsModule } from '@angular/forms';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  declarations: [LoginComponent, CreateAccountComponent],
+  imports: [CommonModule, LoginRoutingModule, FormsModule],
 })
-export class LoginModule { }
+export class LoginModule {}
